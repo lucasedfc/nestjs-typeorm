aac8bcafc38a5357166467d9bef6b35d3e331d95
Lucas Heim <lucasedfc@gmail.com>
2021-07-24 20:49:44 -0300

    Auth decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
 
 @Controller()
+@UseGuards(ApiKeyGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): { [key: string]: string } {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  @Public()
+  test() {
+    return {
+      message: 'Test',
+    };
   }
 
   // @Get('tasks')
