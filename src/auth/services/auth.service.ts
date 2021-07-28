0ef9e155985ee44a6335436dabe71aeb4aaaa9ce
Lucas Heim <lucasedfc@gmail.com>
2021-07-28 19:08:49 -0300

    Passport

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    const isMatch = await bcrypt.compare(password, user?.password || '');
+    if (user && isMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  }
+}
